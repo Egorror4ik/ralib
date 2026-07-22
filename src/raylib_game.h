5e19fffafd8f97606f378d9f1b9829ad84f30c80
Egorror4ik <E.D.Yakovlev@yandex.ru>
2026-07-22 18:12:29 +0300

    Create raylib_game.h

---
+++ src/raylib_game.h
@@ -0,0 +1,11 @@
+// src/raylib_game.h
+#ifndef RAYLIB_GAME_H
+#define RAYLIB_GAME_H
+
+#include "raylib.h"
+
+// Глобальные переменные
+extern Sound fxCoin;  // Объявление, но не определение
+extern Font font;
+
+#endif
